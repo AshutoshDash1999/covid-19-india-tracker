2da5528d2dd5e5e1d3f7dfee432714660346a5a4
Ashutosh Dash <ashu.vampire13@gmail.com>
2021-06-11 08:55:30 +0530

    fixed alignment issues

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,22 @@
-async function getCovidapi(){
-    const jsondata = await fetch('https://api.covid19api.com/summary');
-    const jsdata = await jsondata.json();
-    console.log(jsdata)
-    // console.log(jsdata.Countries[76].Country);
-    TotalConfirmed_global = jsdata.Global.TotalConfirmed;
-    TotalRecovered_global = jsdata.Global.TotalRecovered;
-    TotalDeaths_global = jsdata.Global.TotalDeaths;
-    
-    document.getElementById("worldwide-confirmed").innerHTML = TotalConfirmed_global.toLocaleString('hi');
-    document.getElementById("worldwide-recovered").innerHTML = TotalRecovered_global.toLocaleString('hi');
-    document.getElementById("worldwide-death").innerHTML = TotalDeaths_global.toLocaleString('hi');
-    
+async function getCovidapi() {
+  const jsondata = await fetch("https://api.covid19api.com/summary");
+  const jsdata = await jsondata.json();
+  console.log(jsdata);
+  // console.log(jsdata.Countries[76].Country);
+  TotalConfirmed_global = jsdata.Global.TotalConfirmed;
+  TotalRecovered_global = jsdata.Global.TotalRecovered;
+  TotalDeaths_global = jsdata.Global.TotalDeaths;
+
+  document.getElementById("worldwide-confirmed").innerHTML =
+    TotalConfirmed_global.toLocaleString("hi");
+  document.getElementById("worldwide-recovered").innerHTML =
+    TotalRecovered_global.toLocaleString("hi");
+  document.getElementById("worldwide-death").innerHTML =
+    TotalDeaths_global.toLocaleString("hi");
 }
 getCovidapi();
 
-function dark_mode(){
-    document.body.style.backgroundColor = "#202123";
-    document.getElementById("text").style.color = "#f9f9f9";
+function dark_mode() {
+  document.body.style.backgroundColor = "#202123";
+  document.getElementById("text").style.color = "#f9f9f9";
 }
