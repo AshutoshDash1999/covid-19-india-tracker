e17a660f85cb06bc0090a1236d6f97f58f573e27
Ashutosh Dash <ashu.vampire13@gmail.com>
2021-07-22 13:33:00 +0530

    updated background color

--- assets/js/script.js
+++ assets/js/script.js
@@ -73,7 +73,7 @@ const options = {
   left: "unset", // default: 'unset'
   time: "0.5s", // default: '0.3s'
   mixColor: "#fff", // default: '#fff'
-  backgroundColor: "#e9e9e3", // default: '#fff'
+  backgroundColor: "#f3f2ef", // default: '#fff'
   buttonColorDark: "#100f2c", // default: '#100f2c'
   buttonColorLight: "#fff", // default: '#fff'
   saveInCookies: true, // default: true,
